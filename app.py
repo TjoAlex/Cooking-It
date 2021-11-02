a0d66907267154c3c3cdcd69b1913d2df85ddf2e
TjoAlex <alle.98@live.se>
2021-11-02 12:04:15 +0000

    Add register functionality

--- app.py
+++ app.py
@@ -24,10 +24,28 @@ def get_recipe():
     recipe = mongo.db.recipe.find()
     return render_template("recipe.html", recipe=recipe)
 
-
+#Register code from code Institute link:https://github.com/Code-Institute-Solutions/TaskManagerAuth/blob/main/02-UserAuthenticationAndAuthorization/03-register_functionality/app.py 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html")  
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+    return render_template("register.html")
 
 
 if __name__ == "__main__":

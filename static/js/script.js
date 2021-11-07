f29127fa121d3c9d48bd9451e89caf8b8894321d
TjoAlex <alle.98@live.se>
2021-11-07 12:49:23 +0000

    Add parallax Initialization

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-            $('.sidenav').sidenav({
-                edge: "right"
-            });
-            $('select').formSelect();
-            $('.parallax').parallax();
-            $('.carousel.carousel-slider').carousel({
-                fullWidth: true
-            });
+    $('.sidenav').sidenav({
+        edge: "right"
+    });
+    $('select').formSelect();
+    $('.parallax').parallax();
+});
